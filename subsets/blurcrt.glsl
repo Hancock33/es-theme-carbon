20af297d4f4cd00cb9c05a365f3178be2b015925
Hancock33 <63157658+Hancock33@users.noreply.github.com>
2025-12-13 17:10:51 +0000

    update to master

--- subsets/blurcrt.glsl
+++ subsets/blurcrt.glsl
@@ -5,8 +5,8 @@
 #define COMPAT_ATTRIBUTE in
 #define COMPAT_TEXTURE texture
 #else
-#define COMPAT_VARYING varying 
-#define COMPAT_ATTRIBUTE attribute 
+#define COMPAT_VARYING varying
+#define COMPAT_ATTRIBUTE attribute
 #define COMPAT_TEXTURE texture2D
 #endif
 
@@ -30,17 +30,17 @@ uniform COMPAT_PRECISION vec2 OutputSize;
 uniform COMPAT_PRECISION vec2 TextureSize;
 uniform COMPAT_PRECISION vec2 InputSize;
 
-void main(void)                                    
-{                                                  
+void main(void)
+{
 	gl_Position = MVPMatrix * vec4(VertexCoord.xy, 0.0, 1.0);
-	v_tex       = TexCoord;                           
-	COL0       = COLOR;        
+	v_tex       = TexCoord;
+	COL0       = COLOR;
 	TEX0.xy = TexCoord.xy * 1.00001;
-    invDims=1.0/TextureSize.xy;                       
+    invDims=1.0/TextureSize.xy;
 }
 
 #elif defined(FRAGMENT)
-			
+
 #if __VERSION__ >= 130
 #define COMPAT_VARYING in
 #define COMPAT_TEXTURE texture
@@ -61,7 +61,7 @@ precision mediump float;
 #else
 #define COMPAT_PRECISION
 #endif
-			
+
 COMPAT_VARYING   vec4      COL0;
 COMPAT_VARYING   vec2      v_tex;
 COMPAT_VARYING   vec4      TEX0;
@@ -109,7 +109,7 @@ vec3 mask(float p)
     else if (pos<0.666) {Mask.r=m, Mask.g=1.0, Mask.b=m;}
     else {Mask.r=m, Mask.g=m, Mask.b=1.0;}
     }
-    
+
     return Mask;
 }
 
@@ -129,15 +129,15 @@ float randomValue(vec2 co) {
     return fract(sin(dot(co.xy, vec2(12.9898, 78.233) + 5.0)) * 43758.5453);
 }
 
-void main(void)                                    
-{         
+void main(void)
+{
 	float blurSize = blur;
 	if (blurSize == 0.0) {
 		blurSize = 3.0;
 	}
-	
+
 	int numSamples = 13;
-	
+
 	// Initialize a color accumulator
 	vec4 blurColor = COMPAT_TEXTURE(u_tex, v_tex) * float(numSamples);
 
@@ -146,11 +146,11 @@ void main(void)
 
 	float pixels = 2.0;
 	int total = numSamples;
-	
+
 	for (int i = 0; i < numSamples; i++) {
-		vec2 offset = vec2(cos(float(i) * 3.14159 * 2.0 / float(numSamples)), sin(float(i) * 3.14159 * 2.0 / float(numSamples)));		
-	    for (int b = 1; b < int(blurSize); b++) {	    
-			blurColor += COMPAT_TEXTURE(u_tex, v_tex + offset * float(b) * stepSize * 1.5);	
+		vec2 offset = vec2(cos(float(i) * 3.14159 * 2.0 / float(numSamples)), sin(float(i) * 3.14159 * 2.0 / float(numSamples)));
+	    for (int b = 1; b < int(blurSize); b++) {
+			blurColor += COMPAT_TEXTURE(u_tex, v_tex + offset * float(b) * stepSize * 1.5);
 			total++;
 		}
 	}
@@ -160,14 +160,14 @@ void main(void)
 
 	// Output the final blurred color
 	// FragColor = blurColor;
-	
+
 	vec2 pos = TEX0.xy;
-    vec2 p = pos * TextureSize; 
+    vec2 p = pos * TextureSize;
     vec2 i = floor(p)*1.0001 + 0.5;
     vec2 f = p - i;
     p = (i + 4.0*f*f*f)*invDims;
     p.x = mix(p.x, pos.x, BLUR);
-  
+
    //animate strength
 
     vec3 texel = blurColor.rgb;
@@ -182,12 +182,12 @@ void main(void)
 
     pixelColor = pow(pixelColor,vec3(GAMMA));
     pixelColor= saturation(pixelColor);
-	
+
     FragColor = vec4(pixelColor, 1.0);
 	/*
 	// Noise effect
-    int anim = int(mod(float(FrameCount), 25.0)); 
-  	  
+    int anim = int(mod(float(FrameCount), 25.0));
+
 	vec2 uv = gl_FragCoord.xy / resolution;
     float seed = float(anim);
 
@@ -196,7 +196,7 @@ void main(void)
 
     // Output the final color with noise
     vec4 pxNoised = vec4(vec3(noise), 1.0);
-	
+
 	vec3 finalColor = mix(pixelColor, vec3(noise), 0.1);
 
 	FragColor = vec4(finalColor, 1.0);*/
--- subsets/gaussianblur.glsl
+++ subsets/gaussianblur.glsl
@@ -5,8 +5,8 @@
 #define COMPAT_ATTRIBUTE in
 #define COMPAT_TEXTURE texture
 #else
-#define COMPAT_VARYING varying 
-#define COMPAT_ATTRIBUTE attribute 
+#define COMPAT_VARYING varying
+#define COMPAT_ATTRIBUTE attribute
 #define COMPAT_TEXTURE texture2D
 #endif
 
@@ -23,15 +23,15 @@ COMPAT_ATTRIBUTE vec4 COLOR;
 COMPAT_VARYING   vec2 v_tex;
 COMPAT_VARYING   vec4 v_col;
 
-void main(void)                                    
-{                                                  
+void main(void)
+{
 	gl_Position = MVPMatrix * vec4(VertexCoord.xy, 0.0, 1.0);
-	v_tex       = TexCoord;                           
-	v_col       = COLOR;                           
+	v_tex       = TexCoord;
+	v_col       = COLOR;
 }
 
 #elif defined(FRAGMENT)
-			
+
 #if __VERSION__ >= 130
 #define COMPAT_VARYING in
 #define COMPAT_TEXTURE texture
@@ -63,29 +63,29 @@ uniform   COMPAT_PRECISION vec2      textureSize;
 void main()
 {
 	float Pi = 6.28318530718; // Pi*2
-    
+
     // GAUSSIAN BLUR SETTINGS {{{
     float Directions = 16.0; // BLUR DIRECTIONS (Default 16.0 - More is better but slower)
     float Quality = 4.0; // BLUR QUALITY (Default 4.0 - More is better but slower)
     float Size = 32; // BLUR SIZE (Radius)
     // GAUSSIAN BLUR SETTINGS }}}
-   
+
     vec2 Radius = Size / resolution;
-    
+
     // Normalized pixel coordinates (from 0 to 1)
     vec2 uv = v_tex;
     // Pixel colour
     vec4 Color = COMPAT_TEXTURE(u_tex, uv);
-    
+
     // Blur calculations
     for( float d=0.0; d<Pi; d+=Pi/Directions)
     {
 		for(float i=1.0/Quality; i<=1.0; i+=1.0/Quality)
         {
-			Color += COMPAT_TEXTURE( u_tex, uv+vec2(cos(d),sin(d))*Radius*i);		
+			Color += COMPAT_TEXTURE( u_tex, uv+vec2(cos(d),sin(d))*Radius*i);
         }
     }
-    
+
     // Output to screen
     Color /= Quality * Directions - 15.0;
     FragColor =  Color * v_col;
